1758f0141f23ecdaf04fee582c36813cfa7b4f37
Gotcha7770 <gbakanov.box@gmail.com>
2024-01-16 14:27:34 +0300

    add cross platform Avalonia project

---
+++ TicTacToe.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TicTacToe.Avalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
