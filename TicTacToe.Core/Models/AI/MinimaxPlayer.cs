d1f3383b9f9a888b2ad44ecea87a273b7c6c0418
Gotcha7770 <gbakanov.box@gmail.com>
2024-02-13 21:43:35 +0300

    MinimaxPlayer without implementation

---
+++ TicTacToe.Core/Models/AI/MinimaxPlayer.cs
@@ -0,0 +1,22 @@
+﻿namespace TicTacToe.Models.AI;
+
+internal static class MinimaxPlayer
+{
+    private class X : XPlayer
+    {
+        public X(Func<Field, Task<Cell>> moveStrategy) : base(moveStrategy) { }
+    }
+
+    private class O : OPlayer
+    {
+        public O(Func<Field, Task<Cell>> moveStrategy) : base(moveStrategy) { }
+    }
+    
+    public static XPlayer FromX() => new X(GetNextMove);
+    public static OPlayer FromO() => new O(GetNextMove);
+
+    private static Task<Cell> GetNextMove(Field field)
+    {
+        throw new NotImplementedException();
+    }
+}
