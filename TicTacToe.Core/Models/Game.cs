b690bb4c240bd1268f3449fa732526f4ab4c292e
Gotcha7770 <gbakanov.box@gmail.com>
2024-02-13 21:12:44 +0300

    add Game State and NextMove logic

--- TicTacToe.Core/Models/Game.cs
+++ TicTacToe.Core/Models/Game.cs
@@ -2,22 +2,63 @@
 
 namespace TicTacToe.Models;
 
+public enum GameState
+{
+    InProgress,
+    GameOver,
+    Draw
+}
+
 public class Game
 {
-    private Field _field;
+    private readonly Field _field;
+    private readonly XPlayer _xPlayer;
+    private readonly OPlayer _oPlayer;
 
-    public Game(IPlayer one, IPlayer other)
+    public Game(XPlayer one, OPlayer other)
     {
         _field = new Field();
-        XPlayer = one;
-        OPlayer = other;
+        _xPlayer = one;
+        _oPlayer = other;
+
+        CurrentPlayer = _xPlayer;
     }
 
-    public IPlayer XPlayer { get; }
-    public IPlayer OPlayer { get; }
+    public GameState State { get; private set; }
+    public IPlayer CurrentPlayer { get; private set; }
 
-
-    public void Start()
+    public async Task<bool> NextMove()
     {
+        if (State is GameState.InProgress)
+        {
+            var move = await CurrentPlayer.GetNextMove(_field);
+            _field.Apply(move);
+
+            if (IsDraw())
+            {
+                State = GameState.Draw;
+            }
+            else if (IsWinner(CurrentPlayer))
+            {
+                State = GameState.GameOver;
+            }
+            else
+            {
+                SwitchPlayer();
+            }
+
+            return true;
+        }
+        
+        return false;
     }
+
+    private void SwitchPlayer()
+    {
+        CurrentPlayer = CurrentPlayer == _xPlayer ? _xPlayer : _oPlayer;
+    }
+
+    private bool IsDraw() => _field.GetEmptyCells().IsEmpty();
+
+    private bool IsWinner(IPlayer player) => _field.GetAllLines().Any(x => x.IsAllSymbolsAre(player.Symbol));
 }
