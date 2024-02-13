9d7b68b372da35ee8eabd5322019d2ff15f2b862
Gotcha7770 <gbakanov.box@gmail.com>
2024-02-13 21:41:23 +0300

    simple game simulation

---
+++ TicTacToe.Core.Tests/GameTests.cs
@@ -0,0 +1,17 @@
+﻿using TicTacToe.Models;
+using TicTacToe.Models.AI;
+
+namespace TicTacToe.Core.Tests;
+
+public class GameTests
+{
+    [Fact]
+    public async Task TwoSimplePlayers()
+    {
+        var first = SimpleAiPlayer.FromX();
+        var second = SimpleAiPlayer.FromO();
+        var game = new Game(first, second);
+
+        while (await game.NextMove()) { }
+    }
+}
