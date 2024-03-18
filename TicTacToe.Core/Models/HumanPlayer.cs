5bdd5645df81c9466d9b628cef17ba98a121f75d
Gotcha7770 <gbakanov.box@gmail.com>
2024-03-18 20:21:52 +0300

    add HumanPlayer

---
+++ TicTacToe.Core/Models/HumanPlayer.cs
@@ -0,0 +1,12 @@
+﻿using TicTacToe.Interfaces;
+
+namespace TicTacToe.Models;
+
+public class HumanPlayer : IPlayer
+{
+    public Symbol Symbol { get; }
+    public Task<Move> GetNextMove(Field field, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
