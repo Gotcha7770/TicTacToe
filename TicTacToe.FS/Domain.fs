d464eb740e11a42866de81ff8613a394300e5ca0
Gotcha7770 <gbakanov.box@gmail.com>
2024-04-20 19:53:07 +0300

    implementation from first Scott Wlaschin article

---
+++ TicTacToe.FS/Domain.fs
@@ -0,0 +1,39 @@
+﻿module TicTacTos.FS.Domain
+
+type HorizontalPosition = Left | HCenter | Right
+type VerticalPosition = Top | VCenter | Bottom
+
+type CellPosition = HorizontalPosition * VerticalPosition
+
+type XPosition = XPosition of CellPosition
+type OPosition = OPosition of CellPosition
+
+type Player = PlayerO | PlayerX
+
+type CellState =
+    | Played of Player
+    | Empty
+
+type Cell = {
+    pos : CellPosition
+    state : CellState
+    }
+
+type ValidMovesForPlayerX = XPosition list
+type ValidMovesForPlayerO = OPosition list
+
+// abstract base class
+type GameState() = class end
+
+type MoveResult =
+    | PlayerXToMove of ValidMovesForPlayerX
+    | PlayerOToMove of ValidMovesForPlayerO
+    | GameWon of Player
+    | GameTied
+
+// the "use-cases"
+type NewGame<'GameState> = 'GameState * MoveResult
+type PlayerXMoves<'GameState> = 'GameState * XPosition -> 'GameState * MoveResult
+type PlayerOMoves<'GameState> = 'GameState * OPosition -> 'GameState * MoveResult
+
+type GetCells<'GameState> = 'GameState -> Cell list
---
+++ TicTacToe.FS/Implementation.fs
@@ -0,0 +1,88 @@
+﻿module TicTacToe.FS.Implementation
+
+open TicTacTos.FS.Domain
+
+type GameState = {
+    cells : Cell list
+    }
+
+/// the list of all horizontal positions
+let allHorizPositions = [Left; HCenter; Right]
+
+/// the list of all vertical positions
+let allVertPositions = [Top; VCenter; Bottom]
+
+/// A type to store the list of cell positions in a line
+type Line = Line of CellPosition list
+
+/// a list of the eight lines to check for 3 in a row
+let linesToCheck =
+    let makeHLine v = Line [for h in allHorizPositions do yield (h,v)]
+    let hLines= [for v in allVertPositions do yield makeHLine v]
+
+    let makeVLine h = Line [for v in allVertPositions do yield (h,v)]
+    let vLines = [for h in allHorizPositions do yield makeVLine h]
+
+    let diagonalLine1 = Line [Left,Top; HCenter,VCenter; Right,Bottom]
+    let diagonalLine2 = Line [Left,Bottom; HCenter,VCenter; Right,Top]
+
+    // return all the lines to check
+    [
+        yield! hLines
+        yield! vLines
+        yield diagonalLine1
+        yield diagonalLine2
+    ]
+        
+type TicTacToeAPI<'GameState>  =
+    {
+        newGame : NewGame<'GameState>
+        playerXMoves : PlayerXMoves<'GameState>
+        playerOMoves : PlayerOMoves<'GameState>
+        getCells : GetCells<'GameState>
+    }
+
+/// get the cells from the gameState
+let getCells gameState =
+    gameState.cells
+    
+/// get the cell corresponding to the cell position
+let getCell gameState posToFind =
+    gameState.cells
+    |> List.find (fun cell -> cell.pos = posToFind)
+
+/// update a particular cell in the GameState
+/// and return a new GameState
+let private updateCell newCell gameState =
+
+    // create a helper function
+    let substituteNewCell oldCell =
+        if oldCell.pos = newCell.pos then
+            newCell
+        else
+            oldCell
+
+    // get a copy of the cells, with the new cell swapped in
+    let newCells = gameState.cells |> List.map substituteNewCell
+
+    // return a new game state with the new cells
+    { gameState with cells = newCells }
+
+/// create the state of a new game
+let newGame : NewGame<GameState> =
+    // create initial game state with empty everything
+    let gameState = { cells=[] }
+    let validMoves = []
+    gameState, ValidMovesForPlayerX validMoves
+
+let playerXMoves : PlayerXMoves<GameState> =
+    let newCell = { pos = cellPos; state = Played PlayerX }
+    let newGameState = gameState |> updateCell newCell
+
+// export the functions
+let api = {
+    newGame = newGame
+    playerOMoves = playerOMoves
+    playerXMoves = playerXMoves
+    getCells = getCells
+}
